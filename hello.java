509d5e9854a61e59167c667e3acf90eaccf290a7
jeswin1811 <140978348+jeswin1811@users.noreply.github.com>
2024-07-25 21:57:31 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+/***********************************************************************************
+ *                         Jeswin Thampichan Joseph
+ *            Computer Science and Engineering Student, SJCET, Palai
+ *                        Email: jeswintj16@gmail.com
+ *
+ *    Project Title: Hello World
+ *    Date: 2024-07-25
+ *
+ *    Description: This is a java program to print Hello World.       
+ ***********************************************************************************/
+
+public class hello {
+    public static void main(String[] args){
+    System.out.println("Hello World");
+    }
+}
