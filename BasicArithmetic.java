e5d16d75c0f92ed6d95f26a65e67fbec8b2f1971
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-07-26 20:59:10 +0530

    Create BasicArithmetic.java

---
+++ BasicArithmetic.java
@@ -0,0 +1,21 @@
+/***********************************************************************************
+ *                         Jeswin Thampichan Joseph
+ *            Computer Science and Engineering Student, SJCET, Palai
+ *                        Email: jeswintj16@gmail.com
+ *
+ *    Project Title: Basic Arithmetic Operations.
+ *    Date: 2024-07-26
+ *
+ *    Description: This is a java program performing the basic arithmetic operations 
+ *                 on two numbers wihtout taking input from console.
+ ***********************************************************************************/
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
