442e297a6f730b6eaf1b8bd78d1258c1ab605c0f
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-07-27 14:33:25 +0530

    Add files via upload

---
+++ sumAvg.java
@@ -0,0 +1,21 @@
+/***********************************************************************************
+ *                         Jeswin Thampichan Joseph
+ *            Computer Science and Engineering Student, SJCET, Palai
+ *                        Email: jeswintj16@gmail.com
+ *
+ *    Project Title: Sum and Average of 3 numbers.
+ *    Date: 2024-07-27
+ *
+ *    Description: This is a java program to find the sum and average of three no.s
+ *                 without using I/O.
+ ***********************************************************************************/
+
+ public class sumAvg {
+    public static void main(String[] args){
+        float a=23.24f,b=5.75f,c=23.46f,sum,avg;
+        sum = a+b+c;
+        avg = sum/3;
+        System.out.println("Sum = "+sum);
+        System.out.println("Average = "+avg);
+    }
+}
