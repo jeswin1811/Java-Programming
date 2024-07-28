c68eb89fdb3f08bce8301de58d17da9ab06604e3
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-07-28 16:36:38 +0530

    Add files via upload

---
+++ areaRectangle.java
@@ -0,0 +1,19 @@
+/***********************************************************************************
+ *                         Jeswin Thampichan Joseph
+ *            Computer Science and Engineering Student, SJCET, Palai
+ *                        Email: jeswintj16@gmail.com
+ *
+ *    Project Title: Area of Rectangle.
+ *    Date: 2024-07-28
+ *
+ *    Description: This is a java program to find the area of a rectangle without 
+ *                 using I/O.
+ ***********************************************************************************/
+
+public class areaRectangle {
+    public static void main(String[] args){
+        float l=3.25f,b=7.65f,area;
+        area = l*b;
+        System.out.println("Area = "+area);
+    }
+}
