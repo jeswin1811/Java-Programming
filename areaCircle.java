3a7d71496c83bf8d40ec84005b9442d19119d79e
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-07-29 19:31:40 +0530

    Add files via upload

---
+++ areaCircle.java
@@ -0,0 +1,19 @@
+/***********************************************************************************
+ *                         Jeswin Thampichan Joseph
+ *            Computer Science and Engineering Student, SJCET, Palai
+ *                        Email: jeswintj16@gmail.com
+ *
+ *    Project Title: Area of a Circle.
+ *    Date: 2024-07-29
+ *
+ *    Description: This is a java program to find the area of a circle without  
+ *                 using I/O.
+ ***********************************************************************************/
+
+public class areaCircle{
+    public static void main(String[] args){
+        float r=5.25f,area;
+        area = 3.14f*r*r;
+        System.out.println("Area = "+area);
+    }
+}
