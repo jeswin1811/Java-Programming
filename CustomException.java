3241c46dbeb65e5a4c3eedf8ec9109009ec655e4
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-09-30 15:56:40 +0530

    Create CustomException.java

---
+++ CustomException.java
@@ -0,0 +1,31 @@
+package matrix;
+import java.util.Scanner;
+class AgeException extends Exception{
+	public AgeException(String message) {
+		super(message);
+	}
+}
+
+public class CustomException {
+	public static void validateAge(int age) throws AgeException{
+		if (age<18) {
+			throw new AgeException("Age must be at least 18 years old.");
+		}
+		System.out.println("Eligible to Vote.");
+	}
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your age: ");
+		int age = input.nextInt();
+		try{
+			validateAge(age);
+		}
+		catch(AgeException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			System.out.println("THIS IS INSIDE FINALLY BLOCK");
+		}
+		input.close();
+	}
+}
