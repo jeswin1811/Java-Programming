04b1d7e4041bcf12522d3606d67f89870e3197c3
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-10-08 20:41:10 +0530

    Update WriteToFile.java

--- WriteToFile.java
+++ WriteToFile.java
@@ -1,3 +1,13 @@
+/***********************************************************************************
+ *                         Jeswin Thampichan Joseph
+ *            Computer Science and Engineering Student, SJCET, Palai
+ *                        Email: jeswintj16@gmail.com
+ *
+ *    Project Title: Write contents to a file.	
+ *    Date: 2024-10-07
+ *
+ *    Description: This is a java program to write contents to a file.
+ ***********************************************************************************/
 package matrix;
 import java.io.*;
 public class WriteToFile {
